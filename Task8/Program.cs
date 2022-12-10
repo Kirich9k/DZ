d6feb7c6e151a60fe657c4e020d7cf9d7cceb95a
Kirich <kirya65@gmail.com>
2022-12-10 23:21:44 +0300

    Task8 was accomplished

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите число, чтобы узнать список чётных чисел между ним и цифрой 1: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+for (i = 1; i < n; i++)
+    if (i % 2 == 0)
+    Console.Write($"{i} ");
