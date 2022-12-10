cc4920aea2afdd9d751813d5d5d4f6d5cf0a142e
Kirich <kirya65@gmail.com>
2022-12-10 23:05:09 +0300

    Task6 was accomplished

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите число, чтобы узнать чётное ли оно: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 2 == 0)
+    Console.Write("Да, число чётное");
+else
+    Console.Write("Нет, число нечётное");
