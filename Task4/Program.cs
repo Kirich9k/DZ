497109238824e0e6288f49002f1ef1745d7ab6fb
Kirich <kirya65@gmail.com>
2022-12-10 22:58:06 +0300

    Task4 was accomplished

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+if (n1 > n2 && n1 > n3)
+    Console.Write($"max={n1}");
+else if (n2 > n1 && n2 > n3)
+    Console.Write($"max={n2}");
+else if (n3 > n1 && n3 > n2)
+    Console.Write($"max={n3}");
